044a2baabf70c4d64354ac5902e2628e1be5c0bc
Stefan Bender <sbender@iaa.es>
2023-03-07 15:36:47 +0100

    tests: Initial test for reading L2 files
    Adds a test for inferring the full path name of the (v8) level2 file.
    Not much, but makes sure that the name is reproducible whenever we
    update the function.

---
+++ tests/test_read.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# vim:fileencoding=utf-8
+import pytest
+
+from mipas_noxy.util import get_nc_filename
+
+
+@pytest.mark.parametrize(
+    "res, spec, v, name",
+    [
+        ["R", "NO", "261", "V8R_NO_261/MIPAS-E_IMK.200001.V8R_NO_261.nc"],
+        ["H", "NO2", "61", "V8H_NO2_61/MIPAS-E_IMK.200001.V8H_NO2_61.nc"],
+    ]
+)
+def test_get_nc_filename(res, spec, v, name):
+    fpath = get_nc_filename(
+        "", res, spec, 2000, 1, version=v,
+    )
+    assert fpath == name
