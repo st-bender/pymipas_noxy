70aa98da5e81a88a8e7920131da3703ee814fec9
Stefan Bender <sbender@iaa.es>
2023-03-09 16:20:31 +0100

    tests: Fix unix/windows path separator
    The result full path is hardcoded, uses `os.sep` to adjust between Unix
    and Windows path names while leaving the rest untouched.

--- tests/test_read.py
+++ tests/test_read.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 # vim:fileencoding=utf-8
+from os import sep as dsep
+
 import pytest
 
 from mipas_noxy.util import get_nc_filename
@@ -8,8 +10,8 @@ from mipas_noxy.util import get_nc_filename
 @pytest.mark.parametrize(
     "res, spec, v, name",
     [
-        ["R", "NO", "261", "V8R_NO_261/MIPAS-E_IMK.200001.V8R_NO_261.nc"],
-        ["H", "NO2", "61", "V8H_NO2_61/MIPAS-E_IMK.200001.V8H_NO2_61.nc"],
+        ["R", "NO", "261", "V8R_NO_261" + dsep + "MIPAS-E_IMK.200001.V8R_NO_261.nc"],
+        ["H", "NO2", "61", "V8H_NO2_61" + dsep + "MIPAS-E_IMK.200001.V8H_NO2_61.nc"],
     ]
 )
 def test_get_nc_filename(res, spec, v, name):
