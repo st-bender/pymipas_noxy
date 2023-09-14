epp: Fix weighted average weights dimensions
In some cases the weights can have different dimensions than the data
variables for which we want to calculate the weighted average. Thus,
simply summing the weights does not normalize them correctly.
Converts the data variable to ones with the same shape (and NaNs) and
multiplies the weights with it to broadcast it to a consistent shape.
Then the sums are over the same number of points for both and the
weights are normalized properly.

src/mipas_noxy/helpers.py
@@ -413,7 +413,8 @@ def plot_corr_hist(hist_sds, ch4_var, noy_var, cmap="jet", min_pts=0):
 # %%
 def weighted_zm(ds, dim="time", variable="target", weight_var="weights"):
     # weights = np.cos(np.radians(ds[weight_var]))
-    weights = ds[weight_var]
+    weights = (ds[variable] * 0. + 1.) * ds[weight_var]
+    weights = weights.fillna(0.)
     ww = weights / weights.sum(dim)
     return (ds[variable] * ww).sum(dim)
 