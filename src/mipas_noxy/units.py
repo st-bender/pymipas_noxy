1e27638145d3b56a36399f333338864ff323c065
Stefan Bender <sbender@iaa.es>
2023-11-20 17:52:14 +0100

    epp: Use xarray units module
    Simplifies the units submodule by importing our packaged xarray units
    implementation. Keeps only the non-standard units.

--- src/mipas_noxy/units.py
+++ src/mipas_noxy/units.py
@@ -16,10 +16,8 @@ multiplication and division to keep track of the units.
 from __future__ import absolute_import, division, print_function
 
 # %%
-import xarray as xr
-
-# %%
 from astropy import units as au
+import xarray_simpleunits as xru
 
 
 # %%
@@ -45,68 +43,6 @@ def convert_to_molmol(ds):
 
 
 # %%
-def _get_unit(x):
-    """Infer unit(s) from attributes
-    """
-    if hasattr(x, "unit"):
-        return x.unit
-    elif hasattr(x, "units"):
-        return au.Unit(x.units)
-    return au.dimensionless_unscaled
-
-
-# %%
-def mul_u(a, b):
-    """Multiplication with units
-    """
-    # ret = a * b
-    ret = a._binary_op(b, xr.core._typed_ops.operator.mul)
-    ret_u = _get_unit(a) * _get_unit(b)
-    ret.attrs["units"] = ret_u.si
-    return ret
-
-
-# %%
-def div_u(a, b):
-    """Division with units
-    """
-    # ret = a * b
-    ret = a._binary_op(b, xr.core._typed_ops.operator.truediv)
-    ret_u = _get_unit(a) / _get_unit(b)
-    ret.attrs["units"] = ret_u.si
-    return ret
-
-
-# %%
-def units_to_str(ds):
-    ds = ds.copy()
-    for v in ds.data_vars:
-        if "units" in ds[v].attrs.keys():
-            _u = ds[v].attrs["units"]
-            if not isinstance(_u, str):
-                _u = _u.to_string()
-            ds[v].attrs["units"] = _u
-    return ds
-
-
-# %%
-def add_method(cls):
-    def decorator(func):
-        # func needs to take `self` as the first argument
-        setattr(cls, func.__name__, func)
-        return func # returning func means func can still be used normally
-    return decorator
-
-
-# %%
-@add_method(xr.DataArray)
-def to_unit(a, u):
-    ret = a * _get_unit(a).to(u)
-    ret.attrs["units"] = u
-    return ret
-
-
-# %%
 au.add_enabled_aliases({"degrees_north": au.degree})
 au.add_enabled_aliases({"degrees_east": au.degree})
 au.add_enabled_aliases({"degree_north": au.degree})
@@ -115,7 +51,4 @@ au.add_enabled_aliases({"ppm": 1e-6 * au.mol / au.mol})
 au.add_enabled_aliases({"ppb": 1e-9 * au.mol / au.mol})
 
 # %%
-xr.DataArray.__mul__ = mul_u
-xr.DataArray.__truediv__ = div_u
-# make sure that attributes are kept, that's where we store the units
-xr.set_options(keep_attrs=True)
+xru.init_units()
